2e5bee9dea7772a2dc998f07b2415d4d7d794f74
xghost2021 <rafaeldias44k@gmail.com>
2024-02-12 23:57:08 -0300

    fix button navbar action

--- script/script.js
+++ script/script.js
@@ -1,12 +1,13 @@
-/* show navbar */
-
 const menuButton = document.getElementById("menu");
 const NavBar = document.querySelector(".responsive");
 
 function showNav() {
-    if(NavBar.style.height==="0vh"){
-        NavBar.style.height="30vh";
-    }else{
-        NavBar.style.height="0vh"
+    const computedStyle = window.getComputedStyle(NavBar); 
+    const height = parseFloat(computedStyle.height); 
+
+    if(height === 0) { 
+        NavBar.style.height = "30vh";
+    } else {
+        NavBar.style.height = "0"; 
     }
 }
